e5d3d80c738af4ed012ab4c24eb0efeed9bd94f1
Crying Surrogate <cryingsurogate@gmail.com>
2022-11-19 15:16:31 +0300

    comments

--- nats-ads.go
+++ nats-ads.go
@@ -6,6 +6,10 @@ import (
 
 func initiateNats() {
 	nc, _ := nats.Connect("nats://95.165.107.100:4222")
-	nc.Subscribe("ith.bot.ads", func(msg *nats.Msg) {})
+	nc.Subscribe("ith.bot.ads", func(msg *nats.Msg) {
+		// decode msg
+		// send msg to all chats in cache
+		// reply to nats msg with result
+	})
 
 }
